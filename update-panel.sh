c7235f6594eb8227ad294423cbe6f05d04a1f4e0
Mahmoud <mahmoud.farhadi91@gmail.com>
2023-08-14 10:38:10 +0330

    updated

--- update-panel.sh
+++ update-panel.sh
@@ -3,7 +3,7 @@
 php_code_url="https://github.com/mahmoud-ap/cyber-panel/raw/master/AdminPanel/1.zip"
 
 original_env_file="/var/www/html/panel/.env"
-php_code_dir="/var/www/html/panel/"
+php_code_dir="/var/www/html/panel"
 
 # Backup original .env file contents to a variable
 original_env_content=$(cat "$original_env_file")
@@ -11,13 +11,13 @@ original_env_content=$(cat "$original_env_file")
 # Download PHP code zip file
 curl -L -o /var/www/html/panel/tmp/new-update.zip "$php_code_url"
 
-# Extract PHP code
-unzip -o /var/www/html/panel/tmp/new-update.zip -d "$php_code_dir"
+# # Extract PHP code
+# unzip -o /var/www/html/panel/tmp/new-update.zip -d "$php_code_dir"
 
-# Restore original .env file contents
-echo "$original_env_content" > "$original_env_file"
+# # Restore original .env file contents
+# echo "$original_env_content" > "$original_env_file"
 
-# Clean up temporary files
-rm /var/www/html/panel/tmp/php_code.zip
+# # Clean up temporary files
+# rm /var/www/html/panel/tmp/php_code.zip
 
-echo "PHP code updated and .env content restored."
+# echo "PHP code updated and .env content restored."
